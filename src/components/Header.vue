0b446f4244fe14ce80ad2498415f133835d81969
ics20-072 <52549142+idaeusice@users.noreply.github.com>
2022-09-14 00:23:50 -0700

    added several additional files to set up a shopping cart app with a navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,4 @@
+<script setup>
+    import Nav from './Nav.vue'
+</script>
+
---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import NavLink from '../components/NavLink.vue'
+    defineProps({
+        msg: {
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div class="links">
+        <NavLink />
+    </div>
+</template>
