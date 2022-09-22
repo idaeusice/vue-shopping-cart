4e1b32880edc5b7fdb5deac91bf67167e8423b1b
ics20-072 <52549142+idaeusice@users.noreply.github.com>
2022-09-22 12:20:10 -0700

    various changes and testing vue features

---
+++ src/components/Cart.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import Nav from './Nav.vue'
+
+    defineProps({
+        cartItems: {
+            type: Object,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div>
+        {{ cartItems }}
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -12,3 +12,41 @@ app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
+
+
+export default {
+    data() {
+        return {
+            loading: false,
+            post: null,
+            error: null,
+        }
+    },
+    created() {
+        // watch the params of the route to fetch the data again
+        this.$watch(
+            () => this.$route.params,
+            () => {
+            this.fetchData()
+            },
+            // fetch the data when the view is created and the data is
+            // already being observed
+            { immediate: true }
+        )
+    },
+    methods: {
+        fetchData() {
+            this.error = this.post = null
+            this.loading = true
+            // replace `getPost` with your data fetching util / API wrapper
+            getPost(this.$route.params.id, (err, post) => {
+            this.loading = false
+            if (err) {
+                this.error = err.toString()
+            } else {
+                this.post = post
+            }
+            })
+        },
+    },
+}
